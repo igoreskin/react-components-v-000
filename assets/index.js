97868921e72a49af9ea0abbcd2709b738a8d44b7
Igor Eskin <github email address>
2018-04-05 13:57:46 -0400

    Done.

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 class Comment extends React.Component {
   render() {
     return (
@@ -21,7 +24,7 @@ class BlogPost extends React.Component {
         <Comment content={"A universal truth. - Noam Chomsky"}/>
         <Comment content={"Truth is singular. Its 'versions' are mistruth. - Sonmi-451"}/>
 
-      </div> 
+      </div>
     )
   }
 }
